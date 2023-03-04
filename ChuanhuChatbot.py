d1c84d440d9425d01c17fa1612b497c1678dbe00
chenhengjie <chenhengjie123@gmail.com>
2023-03-04 19:46:32 +0800

    feat: 改回原始的格式化方式

--- ChuanhuChatbot.py
+++ ChuanhuChatbot.py
@@ -17,22 +17,22 @@ if OPENAI_API_KEY == "empty":
 openai.api_key = OPENAI_API_KEY
 
 def parse_text(text):
-    return text
+    # return text
     # return markdown.markdown(text)
-    # lines = text.split("\n")
-    # for i,line in enumerate(lines):
-    #     if "```" in line:
-    #         items = line.split('`')
-    #         if items[-1]:
-    #             lines[i] = f'<pre><code class="{items[-1]}">'
-    #         else:
-    #             lines[i] = f'</code></pre>'
-    #     else:
-    #         if i>0:
-    #             line = line.replace("<", "&lt;")
-    #             line = line.replace(">", "&gt;")
-    #             lines[i] = '<br/>'+line.replace(" ", "&nbsp;")
-    # return "".join(lines)
+    lines = text.split("\n")
+    for i,line in enumerate(lines):
+        if "```" in line:
+            items = line.split('`')
+            if items[-1]:
+                lines[i] = f'<pre><code class="{items[-1]}">'
+            else:
+                lines[i] = f'</code></pre>'
+        else:
+            if i>0:
+                line = line.replace("<", "&lt;")
+                line = line.replace(">", "&gt;")
+                lines[i] = '<br/>'+line.replace(" ", "&nbsp;")
+    return "".join(lines)
 
 def get_response(system, context, raw = False):
     response = openai.ChatCompletion.create(
